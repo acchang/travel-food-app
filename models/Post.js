86450694621373517deccac2e4dfd93070330c41
Andrew Chang <acchang4@gmail.com>
2022-11-16 09:58:36 -0500

    headers

--- models/Post.js
+++ models/Post.js
@@ -5,6 +5,10 @@ const PostSchema = new mongoose.Schema({
       type: String,
       require: true,
     },
+    image: {
+      type: String,
+      require: true,
+    },
     dishName: {
       type: String,
       required: true,
@@ -37,35 +41,6 @@ const PostSchema = new mongoose.Schema({
       type: Date,
       default: Date.now,
     },
-
-  // title: {
-  //   type: String,
-  //   required: true,
-  // },
-  // image: {
-  //   type: String,
-  //   require: true,
-  // },
-  // cloudinaryId: {
-  //   type: String,
-  //   require: true,
-  // },
-  // caption: {
-  //   type: String,
-  //   required: true,
-  // },
-  // likes: {
-  //   type: Number,
-  //   required: true,
-  // },
-  // user: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: "User",
-  // },
-  // createdAt: {
-  //   type: Date,
-  //   default: Date.now,
-  // },
 });
 
 module.exports = mongoose.model("Post", PostSchema);
